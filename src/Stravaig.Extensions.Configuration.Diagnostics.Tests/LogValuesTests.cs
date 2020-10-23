2c9ee8b1880a32e15f3bc69887e956159834c245
Colin Mackay <github@colinmackay.scot>
2020-10-23 23:20:47 +0100

    Add Log Value tests

--- src/Stravaig.Extensions.Configuration.Diagnostics.Tests/LogValuesTests.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics.Tests/LogValuesTests.cs
@@ -31,14 +31,48 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
         public void LogsThreeValuesAsInformation()
         {
             ConfigRoot.LogConfigurationValuesAsInformation(Logger);
+            CheckLog(LogLevel.Information);
+        }
+        
+        [Test]
+        public void LogsThreeValuesAsDebug()
+        {
+            ConfigRoot.LogConfigurationValuesAsDebug(Logger);
+            CheckLog(LogLevel.Debug);
+        }
+        
+        [Test]
+        public void LogsThreeValuesAsTrace()
+        {
+            ConfigRoot.LogConfigurationValuesAsTrace(Logger);
+            CheckLog(LogLevel.Trace);
+        }
 
+        [Test]
+        [TestCase(LogLevel.Critical)]
+        [TestCase(LogLevel.Error)]
+        [TestCase(LogLevel.Warning)]
+        [TestCase(LogLevel.Information)]
+        [TestCase(LogLevel.Debug)]
+        [TestCase(LogLevel.Trace)]
+        public void LogsThreeValuesAsSpecificLogLevel(LogLevel level)
+        {
+            ConfigRoot.LogConfigurationValues(Logger, level);
+
+            CheckLog(level);
+        }
+
+        private void CheckLog(LogLevel level)
+        {
             var logs = GetLogs();
             logs.Count.ShouldBe(1);
-            logs[0].LogLevel.ShouldBe(LogLevel.Information);
-            logs[0].FormattedMessage.ShouldStartWith("The following values are available");
-            logs[0].FormattedMessage.ShouldContain("ConfigOne : One");
-            logs[0].FormattedMessage.ShouldContain("ConfigTwo : Dos");
-            logs[0].FormattedMessage.ShouldContain("ConfigThree : Tres");
+            logs[0].LogLevel.ShouldBe(level);
+            var message = logs[0].FormattedMessage;
+            message.ShouldStartWith("The following values are available");
+            message.ShouldContain("ConfigOne : One");
+            message.ShouldContain("ConfigTwo : Dos");
+            message.ShouldContain("ConfigThree : Tres");
         }
+
     }
 }
