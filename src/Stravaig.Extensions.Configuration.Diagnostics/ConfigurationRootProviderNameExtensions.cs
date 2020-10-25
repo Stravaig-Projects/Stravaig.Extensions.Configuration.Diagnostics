881649f84be193021243ade596ff5c56674a1135
Colin Mackay <github@colinmackay.scot>
2020-10-25 22:58:02 +0000

    Add intellisense documentation

--- src/Stravaig.Extensions.Configuration.Diagnostics/ConfigurationRootProviderNameExtensions.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/ConfigurationRootProviderNameExtensions.cs
@@ -8,27 +8,52 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
 {
     public static class ConfigurationRootProviderNameExtensions
     {
+        /// <summary>
+        /// Logs the provider names in the given <see cref="T:Microsoft.Extensions.Configuration.IConfigurationRoot"/>
+        /// at the Information log level.
+        /// </summary>
+        /// <param name="config">The configuration root to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
         public static void LogProviderNamesAsInformation(this IConfigurationRoot config, ILogger logger)
         {
             config.LogProviderNames(logger, LogLevel.Information);
         }
 
+        /// <summary>
+        /// Logs the provider names in the given <see cref="T:Microsoft.Extensions.Configuration.IConfigurationRoot"/>
+        /// at the Debug log level.
+        /// </summary>
+        /// <param name="config">The configuration root to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
         public static void LogProviderNamesAsDebug(this IConfigurationRoot config, ILogger logger)
         {
             config.LogProviderNames(logger, LogLevel.Debug);
         }
 
+        /// <summary>
+        /// Logs the provider names in the given <see cref="T:Microsoft.Extensions.Configuration.IConfigurationRoot"/>
+        /// at the Trace log level.
+        /// </summary>
+        /// <param name="config">The configuration root to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
         public static void LogProviderNamesAsTrace(this IConfigurationRoot config, ILogger logger)
         {
             config.LogProviderNames(logger, LogLevel.Trace);
         }
 
+        /// <summary>
+        /// Logs the provider names in the given <see cref="T:Microsoft.Extensions.Configuration.IConfigurationRoot"/>
+        /// </summary>
+        /// <param name="config">The configuration root to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
+        /// <param name="level">The log level to use.</param>
         public static void LogProviderNames(this IConfigurationRoot config, ILogger logger, LogLevel level)
         {
             var providerNames = config.Providers
                 .Select(p => p.GetType().FullName);
-            string message = "The following configuration providers were registered:" + Environment.NewLine
-                + string.Join(Environment.NewLine, providerNames);
+            string message = "The following configuration providers were registered:" +
+                             Environment.NewLine +
+                             string.Join(Environment.NewLine, providerNames);
             logger.Log(level, message);
         }
     }
--- src/Stravaig.Extensions.Configuration.Diagnostics/ConnectionStringExtensions.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/ConnectionStringExtensions.cs
@@ -10,6 +10,13 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
 {
     public static class ConnectionStringExtensions
     {
+        /// <summary>
+        /// Logs the details of the named connection string at the desired level.
+        /// </summary>
+        /// <param name="config">The configuration that contains the connection strings.</param>
+        /// <param name="name">The name of the connection string to log.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="level">The level to log at.</param>
         public static void LogConnectionString(this IConfiguration config, string name, ILogger logger, LogLevel level)
         {
             string connectionString = config.GetConnectionString(name);
@@ -26,12 +33,88 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
             }
         }
 
+        /// <summary>
+        /// Logs the details of the named connection string at the information level.
+        /// </summary>
+        /// <param name="config">The configuration that contains the connection strings.</param>
+        /// <param name="name">The name of the connection string to log.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsInformation(this IConfiguration config, string name, ILogger logger)
+        {
+            config.LogConnectionString(name, logger, LogLevel.Information);
+        }
+
+        /// <summary>
+        /// Logs the details of the named connection string at the debug level.
+        /// </summary>
+        /// <param name="config">The configuration that contains the connection strings.</param>
+        /// <param name="name">The name of the connection string to log.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsDebug(this IConfiguration config, string name, ILogger logger)
+        {
+            config.LogConnectionString(name, logger, LogLevel.Debug);
+        }
+
+        /// <summary>
+        /// Logs the details of the named connection string at the trace level.
+        /// </summary>
+        /// <param name="config">The configuration that contains the connection strings.</param>
+        /// <param name="name">The name of the connection string to log.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsTrace(this IConfiguration config, string name, ILogger logger)
+        {
+            config.LogConnectionString(name, logger, LogLevel.Trace);
+        }
+
+        /// <summary>
+        /// Logs the details of the connection's connection string at the given level.
+        /// </summary>
+        /// <param name="connection">The connection object.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="level">The level to log at.</param>
         public static void LogConnectionString(this IDbConnection connection, ILogger logger, LogLevel level)
         {
             var connectionString = connection.ConnectionString;
             logger.LogConnectionString(level, connectionString);
         }
 
+        /// <summary>
+        /// Logs the details of the connection's connection string at the information level.
+        /// </summary>
+        /// <param name="connection">The connection object.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsInformation(this IDbConnection connection, ILogger logger)
+        {
+            connection.LogConnectionString(logger, LogLevel.Information);
+        }
+
+        /// <summary>
+        /// Logs the details of the connection's connection string at the debug level.
+        /// </summary>
+        /// <param name="connection">The connection object.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsDebug(this IDbConnection connection, ILogger logger)
+        {
+            connection.LogConnectionString(logger, LogLevel.Debug);
+        }
+
+        /// <summary>
+        /// Logs the details of the connection's connection string at the trace level.
+        /// </summary>
+        /// <param name="connection">The connection object.</param>
+        /// <param name="logger">The logger to send the details to.</param>
+        public static void LogConnectionStringAsTrace(this IDbConnection connection, ILogger logger)
+        {
+            connection.LogConnectionString(logger, LogLevel.Trace);
+        }
+
+        /// <summary>
+        /// Logs the deconstructed connection string at the desired level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="level">The level to log at.</param>
+        /// <param name="connectionString">The connection string to be deconstructed.</param>
+        /// <param name="connectionStringName">The name of the connection string, or null if there is no name or it is not known.</param>
         public static void LogConnectionString(this ILogger logger, LogLevel level, string connectionString, string connectionStringName = null)
         {
             if (string.IsNullOrWhiteSpace(connectionString))
@@ -51,6 +134,40 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
             var objArgs = args.ToArray();
             logger.Log(level, messageTemplate.ToString(), objArgs);
         }
+
+        /// <summary>
+        /// Logs the deconstructed connection string at the information level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="connectionString">The connection string to be deconstructed.</param>
+        /// <param name="connectionStringName">The name of the connection string, or null if there is no name or it is not known.</param>
+        public static void LogConnectionStringAsInformation(this ILogger logger, string connectionString, string connectionStringName = null)
+        {
+            logger.LogConnectionString(LogLevel.Information, connectionString, connectionStringName);
+        }
+
+        /// <summary>
+        /// Logs the deconstructed connection string at the debug level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="connectionString">The connection string to be deconstructed.</param>
+        /// <param name="connectionStringName">The name of the connection string, or null if there is no name or it is not known.</param>
+        public static void LogConnectionStringAsDebug(this ILogger logger, string connectionString, string connectionStringName = null)
+        {
+            logger.LogConnectionString(LogLevel.Debug, connectionString, connectionStringName);
+        }
+        
+        /// <summary>
+        /// Logs the deconstructed connection string at the trace level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="connectionString">The connection string to be deconstructed.</param>
+        /// <param name="connectionStringName">The name of the connection string, or null if there is no name or it is not known.</param>
+        public static void LogConnectionStringAsTrace(this ILogger logger, string connectionString, string connectionStringName = null)
+        {
+            logger.LogConnectionString(LogLevel.Trace, connectionString, connectionStringName);
+        }
+
 
         private static void AddConnectionStringKeysAndValues(
             StringBuilder messageTemplate,
