409a11332c8bd7fb87bb8e78e8bc9ae2790e1929
Colin Mackay <github@colinmackay.scot>
2020-11-01 15:38:16 +0000

    Add tests for the Regex matcher

--- src/Stravaig.Extensions.Configuration.Diagnostics/Matchers/RegexMatcher.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/Matchers/RegexMatcher.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Text.RegularExpressions;
 
 namespace Stravaig.Extensions.Configuration.Diagnostics.Matchers
 {
+    /// <summary>
+    /// A matcher that uses a regular expression to match to the given key.
+    /// </summary>
     public class RegexMatcher : IMatcher
     {
         private readonly Regex _regex;
-        public RegexMatcher(string regularExpression)
+
+        /// <summary>
+        /// Initialise a Regular Expression matcher that ignores case.
+        /// </summary>
+        /// <param name="regexPattern">The regular expression pattern.</param>
+        public RegexMatcher(string regexPattern)
+            : this (regexPattern, RegexOptions.IgnoreCase)
         {
-            _regex = new Regex(regularExpression, RegexOptions.IgnoreCase);
         }
 
-        public RegexMatcher(string regularExpression, RegexOptions options)
+        /// <summary>
+        /// Initialise a Regular Expression matcher.
+        /// </summary>
+        /// <param name="regexPattern">The regular expression pattern.</param>
+        /// <param name="options">The options for how the regular expression is interpreted.</param>
+        public RegexMatcher(string regexPattern, RegexOptions options)
         {
-            _regex = new Regex(regularExpression, options);
+            if (string.IsNullOrWhiteSpace(regexPattern))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(regexPattern));
+
+            _regex = new Regex(regexPattern, options);
         }
 
         /// <inheritdoc />
