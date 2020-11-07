c83816bfd7a719afce2f4a27318716c5883947a4
Colin Mackay <github@colinmackay.scot>
2020-11-07 13:08:05 +0000

    Obfuscate values logged

--- src/Stravaig.Extensions.Configuration.Diagnostics.Tests/LogValuesTests.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics.Tests/LogValuesTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Shouldly;
+using Stravaig.Extensions.Configuration.Diagnostics.Obfuscators;
 using Stravaig.Extensions.Configuration.Diagnostics.Tests.__data;
 
 namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
@@ -36,12 +37,28 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
         }
         
         [Test]
+        public void LogsThreeValuesWithObfuscationAsInformation()
+        {
+            var options = SetupOptions();
+            ConfigRoot.LogConfigurationValuesAsInformation(Logger, options);
+            CheckObfuscatedLog(LogLevel.Information);
+        }
+        
+        [Test]
         public void LogsThreeValuesAsDebug()
         {
             ConfigRoot.LogConfigurationValuesAsDebug(Logger);
             CheckLog(LogLevel.Debug);
         }
         
+        [Test]
+        public void LogsThreeValuesWithObfuscationAsDebug()
+        {
+            var options = SetupOptions();
+            ConfigRoot.LogConfigurationValuesAsDebug(Logger, options);
+            CheckObfuscatedLog(LogLevel.Debug);
+        }
+
         [Test]
         public void LogsThreeValuesAsTrace()
         {
@@ -50,12 +67,36 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
         }
 
         [Test]
+        public void LogsThreeValuesWithObfuscationAsTrace()
+        {
+            var options = SetupOptions();
+            ConfigRoot.LogConfigurationValuesAsTrace(Logger, options);
+            CheckObfuscatedLog(LogLevel.Trace);
+        }
+
+        [Test]
         [TestCaseSource(typeof(LogLevelSource))]
         public void LogsThreeValuesAsSpecificLogLevel(LogLevel level)
         {
             ConfigRoot.LogConfigurationValues(Logger, level);
-
             CheckLog(level);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(LogLevelSource))]
+        public void LoggedSecretsAreObfuscated(LogLevel level)
+        {
+            var options = SetupOptions();
+            ConfigRoot.LogConfigurationValues(Logger, level, options);
+            CheckObfuscatedLog(level);
+        }
+
+        private static ConfigurationDiagnosticsOptions SetupOptions()
+        {
+            var options = new ConfigurationDiagnosticsOptions();
+            options.BuildConfigurationKeyMatcher(builder => builder.AddContainsMatcher("ConfigThree"));
+            options.Obfuscator = new FixedAsteriskObfuscator();
+            return options;
         }
 
         private void CheckLog(LogLevel level)
@@ -68,6 +109,18 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
             message.ShouldContain("ConfigOne : One");
             message.ShouldContain("ConfigTwo : Dos");
             message.ShouldContain("ConfigThree : Tres");
+        }
+        
+        private void CheckObfuscatedLog(LogLevel level)
+        {
+            var logs = GetLogs();
+            logs.Count.ShouldBe(1);
+            logs[0].LogLevel.ShouldBe(level);
+            var message = logs[0].FormattedMessage;
+            message.ShouldStartWith("The following values are available");
+            message.ShouldContain("ConfigOne : One");
+            message.ShouldContain("ConfigTwo : Dos");
+            message.ShouldContain("ConfigThree : ****");
         }
 
     }
--- src/Stravaig.Extensions.Configuration.Diagnostics/ConfigurationExtensions.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/ConfigurationExtensions.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Stravaig.Extensions.Configuration.Diagnostics
 {
+    /// <summary>
+    /// Extension method for the IConfiguration interface
+    /// </summary>
     public static class ConfigurationExtensions
     {
         /// <summary>
@@ -16,6 +20,17 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
         {
             config.LogConfigurationValues(logger, LogLevel.Information);
         }
+        
+        /// <summary>
+        /// Logs the keys and values in the given configuration object at the information level.
+        /// </summary>
+        /// <param name="config">The configuration object to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
+        /// <param name="options">The options to use to obfuscate secrets.</param>
+        public static void LogConfigurationValuesAsInformation(this IConfiguration config, ILogger logger, ConfigurationDiagnosticsOptions options)
+        {
+            config.LogConfigurationValues(logger, LogLevel.Information, options);
+        }
 
         /// <summary>
         /// Logs the keys and values in the given configuration object at the debug level.
@@ -25,6 +40,17 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
         public static void LogConfigurationValuesAsDebug(this IConfiguration config, ILogger logger)
         {
             config.LogConfigurationValues(logger, LogLevel.Debug);
+        }
+        
+        /// <summary>
+        /// Logs the keys and values in the given configuration object at the debug level.
+        /// </summary>
+        /// <param name="config">The configuration object to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
+        /// <param name="options">The options to use to obfuscate secrets.</param>
+        public static void LogConfigurationValuesAsDebug(this IConfiguration config, ILogger logger, ConfigurationDiagnosticsOptions options)
+        {
+            config.LogConfigurationValues(logger, LogLevel.Debug, options);
         }
 
         /// <summary>
@@ -38,21 +64,58 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
         }
         
         /// <summary>
-        /// Logs the keys and values in the given configuration object.
+        /// Logs the keys and values in the given configuration object at the trace level.
+        /// </summary>
+        /// <param name="config">The configuration object to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
+        /// <param name="options">The options to use to obfuscate secrets.</param>
+        public static void LogConfigurationValuesAsTrace(this IConfiguration config, ILogger logger, ConfigurationDiagnosticsOptions options)
+        {
+            config.LogConfigurationValues(logger, LogLevel.Trace, options);
+        }
+
+        /// <summary>
+        /// Logs the keys and values in the given configuration object using the <see cref="ConfigurationDiagnosticsOptions"/>'s GlobalOptions.
         /// </summary>
         /// <param name="config">The configuration object to examine.</param>
         /// <param name="logger">The logger to write the results to.</param>
         /// <param name="level">The level to log at.</param>
         public static void LogConfigurationValues(this IConfiguration config, ILogger logger, LogLevel level)
         {
+            config.LogConfigurationValues(logger, level, ConfigurationDiagnosticsOptions.GlobalOptions);
+        }
+        
+        /// <summary>
+        /// Logs the keys and values in the given configuration object.
+        /// </summary>
+        /// <param name="config">The configuration object to examine.</param>
+        /// <param name="logger">The logger to write the results to.</param>
+        /// <param name="level">The level to log at.</param>
+        /// <param name="options">The options to use to obfuscate secrets.</param>
+        public static void LogConfigurationValues(this IConfiguration config, ILogger logger, LogLevel level, ConfigurationDiagnosticsOptions options)
+        {
+            
             var valueMap = config.AsEnumerable()
                 .OrderBy(kvp => kvp.Key)
-                .Select(kvp => $"{kvp.Key} : {kvp.Value}");
+                .Select(kvp => Obfuscate(kvp, options))
+                .Select(Render);
             string message = "The following values are available:" +
                              Environment.NewLine +
                              string.Join(Environment.NewLine, valueMap);
             
             logger.Log(level, message);
+        }
+
+        private static string Render(KeyValuePair<string, string> kvp)
+        {
+            return $"{kvp.Key} : {kvp.Value}";
+        }
+
+        private static KeyValuePair<string, string> Obfuscate(KeyValuePair<string, string> kvp, ConfigurationDiagnosticsOptions options)
+        {
+            return options.ConfigurationKeyMatcher.IsMatch(kvp.Key)
+                ? new KeyValuePair<string, string>(kvp.Key, options.Obfuscator.Obfuscate(kvp.Value))
+                : kvp;
         }
     }
 }
