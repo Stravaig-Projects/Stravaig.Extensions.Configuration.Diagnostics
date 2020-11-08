1f3c79e9e0ada19519192d60230df49eb0729a6f
Colin Mackay <github@colinmackay.scot>
2020-11-08 12:12:39 +0000

    Add ConnectionStrings with obfuscation

--- src/Stravaig.Extensions.Configuration.Diagnostics.Tests/ConnectionStringLogTests.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics.Tests/ConnectionStringLogTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Shouldly;
+using Stravaig.Extensions.Configuration.Diagnostics.Obfuscators;
 using Stravaig.Extensions.Configuration.Diagnostics.Tests.__data;
 using Stravaig.Extensions.Configuration.Diagnostics.Tests.__fakes;
 
@@ -37,14 +38,31 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
             Logger.LogConnectionString(level, SqlServerStandardSecurityValue);
             VerifySqlServerStandardSecurityConnectionString(level);
         }
+        
+        [Test]
+        [TestCaseSource(typeof(LogLevelSource))]
+        public void SqlServerStandardSecurityConnectionStringWithObfuscatedPassword(LogLevel level)
+        {
+            var options = SetupOptions();
+            Logger.LogConnectionString(level, SqlServerStandardSecurityValue, null, options);
+            VerifyObfuscatedSqlServerStandardSecurityConnectionString(level);
+        }
 
         [Test]
         [TestCaseSource(typeof(LogLevelSource))]
         public void ConfigSqlServerStandardSecurityConnectionString(LogLevel level)
         {
-            const string name = "SqlServerStandardSecurity";
-            ConfigRoot.LogConnectionString(name, Logger, level);
-            VerifySqlServerStandardSecurityConnectionString(level, name);
+            ConfigRoot.LogConnectionString(SqlServerStandardSecurityKey, Logger, level);
+            VerifySqlServerStandardSecurityConnectionString(level, SqlServerStandardSecurityKey);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(LogLevelSource))]
+        public void ConfigSqlServerStandardSecurityConnectionStringWithObfuscatedPassword(LogLevel level)
+        {
+            var options = SetupOptions();
+            ConfigRoot.LogConnectionString(SqlServerStandardSecurityKey, Logger, level, options);
+            VerifyObfuscatedSqlServerStandardSecurityConnectionString(level, SqlServerStandardSecurityKey);
         }
 
         [Test]
@@ -56,6 +74,13 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
             VerifySqlServerStandardSecurityConnectionString(level);
         }
 
+        private static ConfigurationDiagnosticsOptions SetupOptions()
+        {
+            var options = new ConfigurationDiagnosticsOptions();
+            options.BuildConnectionStringElementMatcher(builder => { builder.AddContainsMatcher("password"); });
+            options.Obfuscator = new MatchedLengthAsteriskObfuscator();
+            return options;
+        }
         private void VerifySqlServerStandardSecurityConnectionString(LogLevel level, string name = null)
         {
             var logs = GetLogs();
@@ -67,6 +92,23 @@ namespace Stravaig.Extensions.Configuration.Diagnostics.Tests
             log.FormattedMessage.ShouldContain("myDataBase");
             log.FormattedMessage.ShouldContain("myUsername");
             log.FormattedMessage.ShouldContain("myPassword");
+            
+            if (!string.IsNullOrWhiteSpace(name))
+                log.FormattedMessage.ShouldContain(name);
+        }
+        
+        private void VerifyObfuscatedSqlServerStandardSecurityConnectionString(LogLevel level, string name = null)
+        {
+            var logs = GetLogs();
+            logs.Count.ShouldBe(1);
+            var log = logs[0];
+            log.LogLevel.ShouldBe(level);
+            log.Exception.ShouldBeNull();
+            log.FormattedMessage.ShouldContain("myServerAddress");
+            log.FormattedMessage.ShouldContain("myDataBase");
+            log.FormattedMessage.ShouldContain("myUsername");
+            log.FormattedMessage.ShouldContain("**********");
+            log.FormattedMessage.ShouldNotContain("myPassword");
             
             if (!string.IsNullOrWhiteSpace(name))
                 log.FormattedMessage.ShouldContain(name);
