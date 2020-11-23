ba3b310871b9f57f1c00fc47fc83f829494dd925
Colin Mackay <github@colinmackay.scot>
2020-11-23 20:48:56 +0000

    #24 Add overrides for specific log levels

--- src/Stravaig.Extensions.Configuration.Diagnostics/ConnectionStringExtensions.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/ConnectionStringExtensions.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Stravaig.Extensions.Configuration.Diagnostics
 {
+    /// <summary>
+    /// Extension methods for deconstructing and logging the components of connection strings.
+    /// </summary>
     public static class ConnectionStringExtensions
     {
         /// <summary>
@@ -67,8 +70,51 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
             logger.LogConnectionString(config, name, LogLevel.Trace, options);
         }
 
+        /// <summary>
+        /// Logs the details of all the connection strings in the configuration at the Information level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="config">The configuration to pick up the connection strings.</param>
+        /// <param name="options">The options defining what elements get obscured.</param>
+        public static void LogAllConnectionStringsAsInformation(this ILogger logger, IConfiguration config,
+            ConfigurationDiagnosticsOptions options = null)
+        {
+            logger.LogAllConnectionStrings(config, LogLevel.Information, options);
+        }        
+
+        /// <summary>
+        /// Logs the details of all the connection strings in the configuration at the Debug level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="config">The configuration to pick up the connection strings.</param>
+        /// <param name="options">The options defining what elements get obscured.</param>
+        public static void LogAllConnectionStringsAsDebug(this ILogger logger, IConfiguration config,
+            ConfigurationDiagnosticsOptions options = null)
+        {
+            logger.LogAllConnectionStrings(config, LogLevel.Debug, options);
+        }        
+
+        /// <summary>
+        /// Logs the details of all the connection strings in the configuration at the Trace level.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="config">The configuration to pick up the connection strings.</param>
+        /// <param name="options">The options defining what elements get obscured.</param>
+        public static void LogAllConnectionStringsAsTrace(this ILogger logger, IConfiguration config,
+            ConfigurationDiagnosticsOptions options = null)
+        {
+            logger.LogAllConnectionStrings(config, LogLevel.Trace, options);
+        }        
+
+        /// <summary>
+        /// Logs the details of all the connection strings in the configuration.
+        /// </summary>
+        /// <param name="logger">The logger to send the details to.</param>
+        /// <param name="config">The configuration to pick up the connection strings.</param>
+        /// <param name="level">The level to log at.</param>
+        /// <param name="options">The options defining what elements get obscured.</param>
         public static void LogAllConnectionStrings(this ILogger logger, IConfiguration config, LogLevel level,
-            ConfigurationDiagnosticsOptions options)
+            ConfigurationDiagnosticsOptions options = null)
         {
             var connectionStringSection = config.GetSection("ConnectionStrings");
             var names = connectionStringSection
@@ -218,7 +264,6 @@ namespace Stravaig.Extensions.Configuration.Diagnostics
         {
             logger.LogConnectionString(LogLevel.Trace, connectionString, connectionStringName);
         }
-
 
         private static void AddConnectionStringKeysAndValues(
             StringBuilder messageTemplate,
