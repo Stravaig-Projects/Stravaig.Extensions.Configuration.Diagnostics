3127b4d6e6ac03a294e370115d73135d4cde1562
Colin Mackay <github@colinmackay.scot>
2020-12-10 21:04:32 +0000

    #35 Add Fluent Extension methods for setting up the options

---
+++ src/Stravaig.Extensions.Configuration.Diagnostics/Extensions/FuncMatcherExtensions.cs
@@ -0,0 +1,51 @@
+using System;
+using Stravaig.Extensions.Configuration.Diagnostics.FluentOptions;
+using Stravaig.Extensions.Configuration.Diagnostics.Matchers;
+
+namespace Stravaig.Extensions.Configuration.Diagnostics
+{
+    /// <summary>
+    /// Extension methods for adding the Func Matcher to the options builder.
+    /// </summary>
+    public static class FuncMatcherExtensions
+    {
+        /// <summary>
+        /// Adds a function predicate to the options builder.
+        /// </summary>
+        /// <param name="builder">The options builder.</param>
+        /// <param name="predicate">The function predicate that determines if a key matches.</param>
+        /// <returns>The options builder</returns>
+        public static IHandOverToNewSectionOptionsBuilder Matching(
+            this IKeyMatcherOptionsBuilder builder,
+            Func<string, bool> predicate)
+        {
+            return builder.Matching(new FuncMatcher(predicate));
+        }
+
+        /// <summary>
+        /// Adds a function predicate to the options builder.
+        /// </summary>
+        /// <param name="builder">The options builder.</param>
+        /// <param name="predicate">The function predicate that determines is a key matches.</param>
+        /// <returns>The options builder.</returns>
+        public static ISubsequentMultipleKeyMatcherOptionsBuilder KeyMatches(
+            this IFirstMultipleKeyMatcherOptionsBuilder builder,
+            Func<string, bool> predicate)
+        {
+            return builder.KeyMatches(new FuncMatcher(predicate));
+        }
+
+        /// <summary>
+        /// Adds a function predicate to the options builder.
+        /// </summary>
+        /// <param name="builder">The options builder.</param>
+        /// <param name="predicate">The function predicate that determines is a key matches.</param>
+        /// <returns>The options builder.</returns>
+        public static ISubsequentMultipleKeyMatcherOptionsBuilder OrMatches(
+            this ISubsequentMultipleKeyMatcherOptionsBuilder builder,
+            Func<string, bool> predicate)
+        {
+            return builder.OrMatches(new FuncMatcher(predicate));
+        }
+    }
+}
