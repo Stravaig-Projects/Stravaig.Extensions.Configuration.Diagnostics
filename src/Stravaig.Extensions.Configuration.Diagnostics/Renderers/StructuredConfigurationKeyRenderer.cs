53c1099ad0bf073765fa7c682284d8cec4b42478
David Fallah <david.fallah@arnoldclark.com>
2020-12-18 14:32:20 +0000

    Implement structured configuration key renderer

---
+++ src/Stravaig.Extensions.Configuration.Diagnostics/Renderers/StructuredConfigurationKeyRenderer.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Stravaig.Extensions.Configuration.Diagnostics.Renderers
+{
+    public class StructuredConfigurationKeyRenderer : Renderer
+    {
+        public static readonly StructuredConfigurationKeyRenderer Instance = new StructuredConfigurationKeyRenderer();
+
+        public MessageEntry Render(IConfiguration configuration, ConfigurationDiagnosticsOptions options)
+        {
+            var configurationValues = configuration.AsEnumerable()
+                .OrderBy(kvp => kvp.Key)
+                .Select(kvp => Obfuscate(kvp, options))
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            
+            List<object> args = new List<object>();
+            StringBuilder messageTemplate = new StringBuilder("The following values are available:" + Environment.NewLine);
+            
+            foreach (var kvp in configurationValues)
+            {
+                var configurationKeyParts = kvp.Key.Split(':');
+                var configurationKeyPlaceholder = Placeholder(configurationKeyParts);
+                messageTemplate.AppendLine($"{kvp.Key} = {configurationKeyPlaceholder}");
+                args.Add(kvp.Value);
+            }
+            
+            var objArgs = args.ToArray();
+            return new MessageEntry(messageTemplate.ToString(), objArgs);
+        }
+        
+        private static KeyValuePair<string, string> Obfuscate(KeyValuePair<string, string> kvp, ConfigurationDiagnosticsOptions options)
+        {
+            return options.ConfigurationKeyMatcher.IsMatch(kvp.Key)
+                ? new KeyValuePair<string, string>(kvp.Key, options.Obfuscator.Obfuscate(kvp.Value))
+                : kvp;
+        }
+    }
+}
