58cf3154ec55ba1dcedd71f1d757572e0f41025d
David Fallah <david.fallah@arnoldclark.com>
2020-12-18 17:55:57 +0000

    Refactor structured configuration key renderer and extract to interface

--- src/Stravaig.Extensions.Configuration.Diagnostics/Renderers/StructuredConfigurationKeyRenderer.cs
+++ src/Stravaig.Extensions.Configuration.Diagnostics/Renderers/StructuredConfigurationKeyRenderer.cs
@@ -1,42 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 
 namespace Stravaig.Extensions.Configuration.Diagnostics.Renderers
 {
-    public class StructuredConfigurationKeyRenderer : Renderer
+    /// <summary>
+    /// A structured renderer that renders all the configuration values in a configuration.
+    /// </summary>
+    public class StructuredConfigurationKeyRenderer : Renderer, IConfigurationKeyRenderer
     {
+        /// <summary>
+        /// The single instance of this class.
+        /// </summary>
         public static readonly StructuredConfigurationKeyRenderer Instance = new StructuredConfigurationKeyRenderer();
 
+        /// <inheritdoc />
         public MessageEntry Render(IConfiguration configuration, ConfigurationDiagnosticsOptions options)
         {
-            var configurationValues = configuration.AsEnumerable()
-                .OrderBy(kvp => kvp.Key)
-                .Select(kvp => Obfuscate(kvp, options))
-                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-            
             List<object> args = new List<object>();
             StringBuilder messageTemplate = new StringBuilder("The following values are available:" + Environment.NewLine);
             
-            foreach (var kvp in configurationValues)
+            foreach (var kvp in configuration.AsEnumerable())
             {
-                var configurationKeyParts = kvp.Key.Split(':');
-                var configurationKeyPlaceholder = Placeholder(configurationKeyParts);
+                var configurationKeyPlaceholder = Placeholder(kvp.Key);
+                var potentiallyObfuscatedValue = Obfuscate(kvp, options);
+                
                 messageTemplate.AppendLine($"{kvp.Key} : {configurationKeyPlaceholder}");
-                args.Add(kvp.Value);
+                args.Add(potentiallyObfuscatedValue);
             }
             
             var objArgs = args.ToArray();
             return new MessageEntry(messageTemplate.ToString(), objArgs);
         }
         
-        private static KeyValuePair<string, string> Obfuscate(KeyValuePair<string, string> kvp, ConfigurationDiagnosticsOptions options)
+        private static string Obfuscate(KeyValuePair<string, string> kvp, ConfigurationDiagnosticsOptions options)
         {
             return options.ConfigurationKeyMatcher.IsMatch(kvp.Key)
-                ? new KeyValuePair<string, string>(kvp.Key, options.Obfuscator.Obfuscate(kvp.Value))
-                : kvp;
+                ? options.Obfuscator.Obfuscate(kvp.Value)
+                : kvp.Value;
         }
     }
 }
